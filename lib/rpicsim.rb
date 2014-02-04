36f3f1bd537cfd63770de93721eaf4681cb56f9f
David (Pololu) <dev-david@pololu.com>
2014-02-03 16:23:13 -0800

    Added a TODO for the have_value matcher

--- lib/rpicsim.rb
+++ lib/rpicsim.rb
@@ -9,3 +9,7 @@ require_relative 'rpicsim/call_stack_info'
 #   time from one point in the program to another
 #   (as a special case, this lets you calculate the iteration time of the
 #     main loop if both points are the same)
+
+# TODO: add matchers: have_value(44), have_value.in(0..3), and maybe:
+#    have_value.between(0, 14)     (like RSpec built-in matcher)
+#    have_value.within(3).of(56)   (like RSpec built-in matcher)
