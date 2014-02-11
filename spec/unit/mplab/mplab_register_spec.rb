ac6406df6d90da5bf7c92c51999ee9e5ed0e1825
David (Pololu) <dev-david@pololu.com>
2014-02-10 16:38:12 -0800

    Added mplab_register_spec.rb, which is a complete test of MplabRegister.

---
+++ spec/unit/mplab/mplab_register_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../../spec_helper'
+
+describe RPicSim::Mplab::MplabRegister do
+  let(:register) { double('register') }
+  subject(:mplab_register) { described_class.new(register) }
+  
+  describe '#write' do
+    it 'writes to the register and returns the value written' do
+      expect(register).to receive(:write).with(40).and_return { nil }
+      return_value = mplab_register.write(40)
+      expect(return_value).to eq 40
+    end
+  end
+  
+  describe '#read' do
+    it 'reads from the register' do
+      expect(register).to receive(:read).and_return { 40 }
+      expect(mplab_register.read).to eq 40
+    end
+  end
+  
+  describe '#name' do
+    it 'just defers to getName' do
+      expect(register).to receive(:getName).and_return { 'LATA' }
+      expect(mplab_register.name).to eq 'LATA'
+    end
+  end
+
+  describe '#address' do
+    it 'just defers to getAddress' do
+      expect(register).to receive(:getAddress).and_return { 2 }
+      expect(mplab_register.address).to eq 2
+    end
+  end
+  
+end
