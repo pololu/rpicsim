505445ccb3dfc70d55710571c739f630b706233e
David (Pololu) <dev-david@pololu.com>
2014-02-15 13:29:44 -0800

    Improved the tests in program_file_spec.rb. by putting GOTO in the right section and adding string tests for the instructions with f and a fields.

--- spec/integration/program_file_spec.rb
+++ spec/integration/program_file_spec.rb
@@ -46,14 +46,19 @@ describe 'RPicSim disassembly' do
     end
   end
   
-  shared_examples_for 'instruction with fields F and A' do
+  shared_examples_for 'instruction with fields f and a' do
+    string = metadata[:opcode] + ' 0x4, ACCESS'
+    it "has string #{string}'" do
+      expect(instruction0.string).to eq string
+    end
+  
     it 'can properly decode all fields' do
       expect(instruction0.operands).to eq(f: 4, a: 0)
       expect(instruction1.operands).to eq(f: 5, a: 1)
     end
   end
   
-  shared_examples_for 'instruction with fields F, D, and A' do
+  shared_examples_for 'instruction with fields f, d, and a' do
     string = metadata[:opcode] + ' 0x4, F, ACCESS'
     it "has string #{string}'" do
       expect(instruction0.string).to eq string
@@ -62,6 +67,18 @@ describe 'RPicSim disassembly' do
     it 'can properly decode all fields' do
       expect(instruction0.operands).to eq(f: 4, d: 1, a: 0)
       expect(instruction1.operands).to eq(f: 5, d: 0, a: 1)
+    end
+  end
+  
+  shared_examples_for 'instruction with field k' do
+    string = metadata[:opcode] + ' 0x2'
+    it "has string #{string}'" do
+      expect(instruction0.string).to eq string
+    end
+
+    it 'k operand with a word address in it' do
+      expected_k = 2 / example.metadata[:address_increment]
+      expect(instruction0.operands).to eq(k: expected_k)
     end
   end
   
@@ -74,42 +91,43 @@ describe 'RPicSim disassembly' do
     
       describe 'ADDWF', opcode: 'ADDWF' do
         it_behaves_like 'instruction'
-        it_behaves_like 'instruction with fields F, D, and A'
+        it_behaves_like 'instruction with fields f, d, and a'
       end
       
       describe 'ADDWFC', opcode: 'ADDWFC' do
         it_behaves_like 'instruction'
-        it_behaves_like 'instruction with fields F, D, and A'    
-      end
-      
-      describe 'GOTO', opcode: 'GOTO' do
-        it_behaves_like 'instruction', size: 4, string: 'GOTO 0x2'
-        
-        it 'has a k operand with a word address in it' do
-          expect(instruction0.operands).to eq(k: 1)
-        end
-        
-        it 'leads to the instruction specified by k' do
-          expect(instruction0.next_addresses).to eq [2]
-        end
+        it_behaves_like 'instruction with fields f, d, and a'    
       end
 
       describe 'CPFSEQ', opcode: 'CPFSEQ' do
         it_behaves_like 'instruction', string: 'CPFSEQ 0x4, ACCESS'
-        it_behaves_like 'instruction with fields F and A'
+        it_behaves_like 'instruction with fields f and a'
         it_behaves_like 'conditional skip'
       end
       
       describe 'CPFSGT', opcode: 'CPFSGT' do
         it_behaves_like 'instruction', string: 'CPFSGT 0x4, ACCESS'
-        it_behaves_like 'instruction with fields F and A'
+        it_behaves_like 'instruction with fields f and a'
         it_behaves_like 'conditional skip'
       end
       
       describe 'CPFSLT', opcode: 'CPFSLT' do
         it_behaves_like 'instruction', string: 'CPFSLT 0x4, ACCESS'
-        it_behaves_like 'instruction with fields F and A'
+        it_behaves_like 'instruction with fields f and a'
         it_behaves_like 'conditional skip'
+      end
+
+    end
+    
+    context 'control oeprations' do
+          
+      describe 'GOTO', opcode: 'GOTO' do
+        it_behaves_like 'instruction', size: 4
+        it_behaves_like 'instruction with field k'
+        
+        it 'leads to the instruction specified by k' do
+          expect(instruction0.next_addresses).to eq [2]
+        end
       end
 
     end
