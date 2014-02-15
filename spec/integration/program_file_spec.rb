13e5f9b1003dfe5709338247eebe576331923a6c
David (Pololu) <dev-david@pololu.com>
2014-02-15 12:10:15 -0800

    Refactored the tests in program_file_spec.rb in anticipation of making a lot more of them and not wanting too much repetition.

--- spec/integration/program_file_spec.rb
+++ spec/integration/program_file_spec.rb
@@ -48,61 +48,61 @@ describe RPicSim::ProgramFile do
 end
 
 describe 'RPicSim disassembly' do
-  let(:address) { @program_file.label('ins_' + instruction_name).address }
+  let(:opcode) { example.metadata[:opcode] }
+  let(:address) { @program_file.label('ins_' + opcode.downcase).address }
   
   let(:instruction0) { @program_file.instruction(address) }
   let(:instruction1) { @program_file.instruction(address + 1) }
+  
+  shared_examples_for 'instruction' do
+    it 'has the right address' do
+      expect(instruction0.address).to eq address
+    end
+      
+    it 'has the right opcode' do
+      expect(instruction0.opcode).to eq opcode
+    end
+
+    it 'has the right size' do
+      # the PIC18 stores flash addresses in terms of bytes, not words
+      expect(instruction0.size).to eq example.metadata.fetch(:size, 2)
+    end
+  end
   
   context 'for PIC18 architecture' do
     before(:all) do
       @program_file = RPicSim::ProgramFile.new(Firmware::Test18F25K50.filename, Firmware::Test18F25K50.device)
     end
     
-    conditional_skips_fa = %w{ cpfseq }
+    conditional_skips_fa = %w{ CPFSEQ }
     
-    describe 'goto' do
-      let(:instruction_name) { 'goto' }
-      
-      it 'has the right address' do
-        expect(instruction0.address).to eq address
-      end
-      
-      it 'has the opcode GOTO' do
-        expect(instruction0.opcode).to eq 'GOTO'
-      end
+    describe 'GOTO', opcode: 'GOTO', size: 4 do
+      it_behaves_like 'instruction'
       
       it 'has a k operand with a word address in it' do
         # the assembly was "goto 2"
         expect(instruction0.operands).to eq('k' => 1)
       end
       
-      it 'has a size of 4 because it takes 2 words' do
-        # the PIC18 stores flash addresses in terms of bytes, not words
-        expect(instruction0.size).to eq 4
-      end
-      
       it 'leads into the right instruction' do
         expect(instruction0.next_addresses).to eq [2]
       end
       
-      it 'includes the Goto module' do
-        expect(instruction0).to be_a_kind_of RPicSim::Instruction::Goto
-      end
     end
     
     conditional_skips_fa.each do |name|
       specify name do
-        address = @program_file.label('ins_' + name).address
+        address = @program_file.label('ins_' + name.downcase).address
         i0 = @program_file.instruction(address)
         expect(i0.address).to eq address
-        expect(i0.opcode).to eq name.upcase
+        expect(i0.opcode).to eq name
         expect(i0.operands).to eq('f' => 4, 'a' => 0)
         expect(i0.size).to eq 2
         expect(i0).to be_a_kind_of RPicSim::Instruction::ConditionalSkip
         expect(i0.next_addresses).to eq [address + 2, address + 4]
         
         i1 = @program_file.instruction(address + 2)
-        expect(i1.opcode).to eq name.upcase
+        expect(i1.opcode).to eq name
         expect(i1.operands).to eq('f' => 5, 'a' => 1)
       end
     end
