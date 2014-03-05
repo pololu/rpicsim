1aed342f3c06d114dbb3e41428c093d3cba49765
David (Pololu) <dev-david@pololu.com>
2014-03-05 10:50:58 -0800

    spec/unit/mplab_memory_spec.rb: Added unit tests for read_byte and write_byte.

--- spec/unit/mplab/mplab_memory_spec.rb
+++ spec/unit/mplab/mplab_memory_spec.rb
@@ -4,13 +4,30 @@ describe RPicSim::Mplab::MplabMemory do
   let(:memory) { double('memory') }
   subject(:mplab_memory) { described_class.new(memory) }
   
-  # TODO: test all the new methods here
+  describe '#read_byte' do
+    it 'reads a single, unsigned byte' do
+      expect(memory).to receive(:Read) do |address, size, bytes|
+        expect(address).to eq 12
+        expect(size).to eq 1
+        expect(bytes).to be_a_kind_of Java::byte[]
+        expect(bytes.size).to eq 1
+        bytes.ubyte_set(0, 0x9B)
+      end
+      expect(mplab_memory.read_byte(12)).to eq 0x9B
+    end
+  end
   
-  describe '#write_word' do
-    it 'writes to memory and returns the value written' do
-      expect(memory).to receive(:WriteWord).with(12, 40).and_return { nil }
-      return_value = mplab_memory.write_word(12, 40)
-      expect(return_value).to eq 40
+  describe '#write_byte' do
+    it 'writes a single, unsigned byte and returns the value written' do
+      expect(memory).to receive(:Write) do |address, size, bytes|
+        expect(address).to eq 12
+        expect(size).to eq 1
+        expect(bytes).to be_a_kind_of Java::byte[]
+        expect(bytes.size).to eq 1
+        expect(bytes.ubyte_get(0)).to eq 0x9B
+      end
+      return_value = mplab_memory.write_byte(12, 0x9B)
+      expect(return_value).to eq 0x9B
     end
   end
   
@@ -18,6 +35,14 @@ describe RPicSim::Mplab::MplabMemory do
     it 'reads memory' do
       expect(memory).to receive(:ReadWord).with(12).and_return { 40 }
       expect(mplab_memory.read_word(12)).to eq 40
+    end
+  end
+
+  describe '#write_word' do
+    it 'writes to memory and returns the value written' do
+      expect(memory).to receive(:WriteWord).with(12, 40).and_return { nil }
+      return_value = mplab_memory.write_word(12, 40)
+      expect(return_value).to eq 40
     end
   end
   
