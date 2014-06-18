505d951c167f8891829d1f983f58fe721c0e9af1
David (Pololu) <dev-david@pololu.com>
2014-06-17 21:46:32 -0700

    Improved rspec_runner.rb so it takes a requirement spec.

--- spec/rspec_runner.rb
+++ spec/rspec_runner.rb
@@ -1,12 +1,15 @@
-# Usage: ruby rspec_runner.rb VERSION [args to rspec]
-version_string = ARGV.shift
-version = Gem::Version.new version_string
-version_specifier = "~> #{version_string}"
-gem 'rspec', version_specifier
-gem 'rspec-core', version_specifier
-gem 'rspec-expectations', version_specifier
-gem 'rspec-mocks', version_specifier
-if version.segments.first >= 3
-  gem 'rspec-support', version_specifier
+# Usage: ruby rspec_runner.rb VERSION_REQ [args to rspec]
+# Some examples:
+#   ruby rspec_runner.rb '~> 2.99.0'  # Uses RSpec 2.99.x
+#   ruby rspec_runner.rb '~> 3.0.0'   # Uses RSpec 3.0.x
+
+version_requirement = Gem::Requirement.new ARGV.shift
+major_version = version_requirement.requirements[0][1].segments[0]
+gem 'rspec', version_requirement
+gem 'rspec-core', version_requirement.to_s
+gem 'rspec-expectations', version_requirement
+gem 'rspec-mocks', version_requirement
+if major_version >= 3
+  gem 'rspec-support', version_requirement
 end
-load Gem.bin_path('rspec-core', 'rspec') #, version_specifier)
+load Gem.bin_path('rspec-core', 'rspec', version_requirement)
