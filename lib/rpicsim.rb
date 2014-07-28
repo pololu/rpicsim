0756d7087e34cbfa016376b702dec12499649976
David (Pololu) <dev-david@pololu.com>
2014-07-27 21:01:05 -0700

    Removed 3 TODOs from earlier which turned out to be bad ideas.

--- lib/rpicsim.rb
+++ lib/rpicsim.rb
@@ -3,7 +3,3 @@ require_relative 'rpicsim/sim'
 require_relative 'rpicsim/call_stack_info'
 require_relative 'rpicsim/xc8_sym_file'
 
-# TODO: deprecate or remove ProgramFile#labels, prolly replaced with Sim#labels
-# TODO: allow stack traces to use labels that didn't come from the ProgramFile
-# TODO: probably deprecate ProgramFile.address_description and do that somewhere else
-#  because the ProgramFile no longer has a complete idea of all the symbols
